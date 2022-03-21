dad059e6806d1c72ef89d40ebae96878a83189d2
victoria makena <makenavictoria1@gmail.com>
2022-03-21 11:56:08 +0300

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-$(function(){
-    $(".flip").flip({
-        trigger: 'hover'
-    });
-});
+//flip the cards
 
-//create prototy
+// $(function () {
+//     $(".flip").flip({
+//         trigger: 'hover'
+//     });
+// });
+
+//constructor
+function Pizza(size, crust, toppings, quantity){
+    this.size=size;
+    this.crust=crust;
+    this.toppings=toppings;
+    this.quantity=quantity;
+}
+    
+var inputSize=parseInt(document.getElementById('size').value);
+var inpuCrust=parseInt(document.getElementById('crust').value);
+var inputToppings=parseInt(document.getElementById('toppings').value);
+var inputDeliver = parseInt(document.getElementById('deliver').value);
+var howMany = parseInt(document.getElementById('quantity').value);
+
+console.log (inputToppings)
