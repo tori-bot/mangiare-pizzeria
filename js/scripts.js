af4fa981360961bd535c3c64b81a62f604f3a89c
victoria makena <makenavictoria1@gmail.com>
2022-03-21 19:56:59 +0300

    add constructor

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,29 @@
 //     });
 // });
 
+var price, crustPrice, toppingPrice, deliveryPrice
+let total=0
+
 //constructor
-function Pizza(size, crust, toppings, quantity){
+function Pizza(size, crust, toppings, quantity,delivery){
     this.size=size;
     this.crust=crust;
     this.toppings=toppings;
     this.quantity=quantity;
+    this.delivery = delivery;
 }
+
+$(document).ready(function () {
+    $("#orderbtn").click(function (event) {
+        var pizzaSize = $("#size option:selected").value;
+        var pizzaCrust = $(".#crust option:selected").value;
+        var pizzaTopping = $(".#toppings option:selected").value;
+    })
+ })
     
+ 
+var newOrder = new Pizza(type, size, crust, toppings, quantity, delivery);
+  
 var inputSize=parseInt(document.getElementById('size').value);
 var inpuCrust=parseInt(document.getElementById('crust').value);
 var inputToppings=parseInt(document.getElementById('toppings').value);
